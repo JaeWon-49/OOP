5f413d79f3fa41bdbfb059e69c957f5e726a07fe
J.JaeWon <118151347+JaeWon-49@users.noreply.github.com>
2023-06-02 16:54:37 +0900

    Update p9inheritMain.cpp

--- p9inheritMain.cpp
+++ p9inheritMain.cpp
@@ -1,19 +1,13 @@
-// DougFO Play Jan. 19, 2022
 #include <iostream>
-#include "ani.h"
-#include "hum.h"
 #include "fish.h"
 using namespace std;
 int main() {
-	animal a;
-	human b;
+	animal* a[3];
+	for (int i = 0; i < 2; i++) a[i] = new fish;
 	fish c;
-
-	a.eat();
-	b.eat();
-	c.eat();
-	b.walk();
+	a[2] = &c;
 	c.swim();
-	cout << " b " << b.nLegs << "\n";
+	//a[2]->swim();
+	((fish*)a[2])->swim();
 	return 123;
 }
