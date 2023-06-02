5316e3f7c88ee4f7ddeb6db57bc81147789a9d5f
J.JaeWon <118151347+JaeWon-49@users.noreply.github.com>
2023-06-02 16:50:19 +0900

    Update lab9S6.cpp

--- lab9S6.cpp
+++ lab9S6.cpp
@@ -3,12 +3,10 @@ using namespace std;
 #include "CarS6.h"
 int main()
 {
-	Car A = Car();
-	Car B = Car();
-	Car C = A + B;
-	cout << A.price << " " << A.speed << endl;
-	cout << B.price << " " << B.speed << endl;
-	cout << C.price << " " << C.speed << endl;
+	Car a(50, 4, 2000), b(70, 2, 1000), c;
+	c = a + b;
+	cout << a.speed << " a speed \n" << a.wheels << " a wheels \n" << a.price << " a price \n" << endl;
+	cout << b.speed << " b speed \n" << b.wheels << " b wheels \n" << b.price << " b price \n" << endl;
+	cout << c.speed << " total speed \n" << c.wheels << " total wheels \n" << c.price << " total price \n" << endl;
 
-	return 123;
 }
