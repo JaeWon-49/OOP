a634a12a174a23ec7107c8ee787540a752ea2023
J.JaeWon <118151347+JaeWon-49@users.noreply.github.com>
2023-06-09 17:38:34 +0900

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,37 +1,22 @@
 #pragma once
-#pragma once
 #include <iostream>
 #include <vector>
+#include <fstream>
 #include <Windows.h>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 class queue : public vector <int> {
 public:
 	// constructor
 	queue() : NI(10), NO(20), NC(100) {
-		this->reserve(NC);
-		totalN = totalOut = 0;
+		reserve(NC);
 	}
 	queue(int ni, int no, int x) : NI(ni), NO(no), NC(x) {
-		this->reserve(NC);
-		totalN = totalOut = 0;
+		reserve(NC);
 	}
 	// member data
 	int NI, NO, NC;
-	int totalN, totalOut;
-	// member function
-	void newin() {
-		int n = rand() % NI;
-		totalN += n;
-		cout << " + " << n << endl;
-		for (int i = 0; i < n; i++) this->push_back(rand() % 100);
-	}
-	void newout() {
-		int m = rand() % NO;
-		totalOut += m;
-		cout << " - " << m << endl;
-		for (int i = 0; i < m; i++) {
-			if (this->size() != 0) this->erase(this->begin());
-		}
-	}
+	int lost;
 };
